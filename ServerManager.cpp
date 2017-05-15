adb3e145f6ed3371bc4f8d33c85422da46ad112f
Dawid Świnoga <dawid.swinoga@gmail.com>
2017-05-16 00:52:55 +0200

    Implement server manager.

--- ServerManager.cpp
+++ ServerManager.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <Ice/Ice.h>
+#include "chatI.h"
 
 using namespace std;
+using namespace Chat;
 
-int main() {
-    cout << "hello world";
+
+int main(int argc, char *argv[]) {
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    string text;
+    try {
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectAdapterPtr adapter =
+                ic->createObjectAdapterWithEndpoints("ServerManagerAdapter", "default -p 10003");
+        GroupServerManagerPtr groupServerManagerPtr = new GroupServerManagerI();
+        GroupServerManagerPrx groupServerManagerPrx =
+                GroupServerManagerPrx::uncheckedCast(adapter->addWithUUID(groupServerManagerPtr));
+        adapter->activate();
+        Ice::ObjectPrx base = ic->stringToProxy("ServerChat:default -p 10000");
+        ChatServerPrx chatServerPrx = ChatServerPrx::checkedCast(base);
+        if (!chatServerPrx)
+            throw "Invalid proxy";
+
+        chatServerPrx->registerServer(groupServerManagerPrx);
+        do {
+            cin >> text;
+        } while (text != "exit");
+        chatServerPrx->unregisterServer(groupServerManagerPrx);
+        ic->shutdown();
+        ic->waitForShutdown();
+    } catch (const Ice::Exception &ex) {
+        cerr << ex << endl;
+        status = 1;
+    } catch (const char *msg) {
+        cerr << msg << endl;
+        status = 1;
+    }
+    if (ic)
+        ic->destroy();
+    return status;
 }
